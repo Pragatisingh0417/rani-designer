6777d36dcdc5af4a348e34218dc177c94eb417a6
pragati <pragatisingh0417@gmail.com>
2026-03-19 12:33:09 +0530

    uopdated

--- src/app/api/uploads/route.ts
+++ src/app/api/uploads/route.ts
@@ -22,6 +22,6 @@ export async function POST(req: Request) {
 
   return NextResponse.json({
     // url: `/uploads/${fileName}`,
-    url: `https://your-backend.onrender.com/uploads/${fileName}` // ✅ CORRECT
+    url: `https://rani-backend.onrender.com/uploads/${fileName}` // ✅ CORRECT
   });
 }
